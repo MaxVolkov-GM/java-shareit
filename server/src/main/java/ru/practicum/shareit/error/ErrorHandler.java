e2ab16cc68446417246e860d65b59d437bda3eab
MaxVolkov-GM <mrdarkvolkov@gmail.com>
2026-05-05 17:38:35 +0300

    fix

--- server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
+++ server/src/main/java/ru/practicum/shareit/error/ErrorHandler.java
@@ -5,31 +5,35 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.exception.ConflictException;
+import ru.practicum.shareit.exception.NotFoundException;
+import ru.practicum.shareit.exception.ValidationException;
 
 import java.util.Map;
 
 @RestControllerAdvice
 public class ErrorHandler {
 
-	@ExceptionHandler(IllegalArgumentException.class)
+	@ExceptionHandler({
+			IllegalArgumentException.class,
+			ValidationException.class
+	})
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public Map<String, String> handleBadRequest(RuntimeException e) {
 		return Map.of("error", e.getMessage());
 	}
 
-	@ExceptionHandler(IllegalStateException.class)
+	@ExceptionHandler({
+			IllegalStateException.class,
+			ConflictException.class,
+			DataIntegrityViolationException.class
+	})
 	@ResponseStatus(HttpStatus.CONFLICT)
-	public Map<String, String> handleConflict(IllegalStateException e) {
+	public Map<String, String> handleConflict(RuntimeException e) {
 		return Map.of("error", e.getMessage());
 	}
 
-	@ExceptionHandler(DataIntegrityViolationException.class)
-	@ResponseStatus(HttpStatus.CONFLICT)
-	public Map<String, String> handleDataIntegrityViolation(DataIntegrityViolationException e) {
-		return Map.of("error", e.getMessage());
-	}
-
-	@ExceptionHandler(RuntimeException.class)
+	@ExceptionHandler(NotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public Map<String, String> handleNotFound(RuntimeException e) {
 		return Map.of("error", e.getMessage());
--- server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
+++ server/src/main/java/ru/practicum/shareit/request/ItemRequestController.java
@@ -8,39 +8,31 @@ import ru.practicum.shareit.request.service.ItemRequestService;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/requests")
+@RequestMapping("/requests")
 @RequiredArgsConstructor
 public class ItemRequestController {
 
 	private final ItemRequestService itemRequestService;
 
 	@PostMapping
-	public ItemRequestDto create(
-			@RequestHeader("X-Sharer-User-Id") Long userId,
-			@RequestBody ItemRequestDto itemRequestDto
-	) {
+	public ItemRequestDto create(@RequestHeader("X-Sharer-User-Id") Long userId,
+	                             @RequestBody ItemRequestDto itemRequestDto) {
 		return itemRequestService.create(userId, itemRequestDto);
 	}
 
 	@GetMapping
-	public List<ItemRequestDto> getUserRequests(
-			@RequestHeader("X-Sharer-User-Id") Long userId
-	) {
+	public List<ItemRequestDto> getUserRequests(@RequestHeader("X-Sharer-User-Id") Long userId) {
 		return itemRequestService.getUserRequests(userId);
 	}
 
 	@GetMapping("/all")
-	public List<ItemRequestDto> getAllRequests(
-			@RequestHeader("X-Sharer-User-Id") Long userId
-	) {
+	public List<ItemRequestDto> getAllRequests(@RequestHeader("X-Sharer-User-Id") Long userId) {
 		return itemRequestService.getAllRequests(userId);
 	}
 
 	@GetMapping("/{requestId}")
-	public ItemRequestDto getById(
-			@RequestHeader("X-Sharer-User-Id") Long userId,
-			@PathVariable Long requestId
-	) {
+	public ItemRequestDto getById(@RequestHeader("X-Sharer-User-Id") Long userId,
+	                              @PathVariable Long requestId) {
 		return itemRequestService.getById(userId, requestId);
 	}
 }
